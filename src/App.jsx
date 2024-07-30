b8a4ecaafe21e6d9ba36738fc43682e9dbfaec92
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-07-30 16:27:57 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BasicTable from './components/BasicTable'
+
+function App() {
+  return (
+    <>
+      <h1>React-table</h1>
+      <BasicTable/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,104 @@
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import mData from "../MOCK_DATA.json";
+import { useMemo } from "react";
+
+function BasicTable() {
+  // create instance of our data next
+  const data = useMemo(() => mData, []);
+
+  // create instance to define our columns
+  /**  @type import('@tanstack/react-table').ColumnDef<any> */
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+      footer: "ID",
+    },
+    {
+      header: "First Name",
+      accessorKey: "first_name",
+      footer: "First Name",
+    },
+    {
+      header: "Last Name",
+      accessorKey: "last_name",
+      footer: "Last Name",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+      footer: "Email",
+    },
+    {
+      header: "Gender",
+      accessorKey: "gender",
+      footer: "Gender",
+    },
+    {
+      header: "Date of Birth",
+      accessorKey: "dob",
+      footer: "Date of Birth",
+    },
+  ];
+
+  // create instance of our table first
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="w3-container">
+      <table className="w3-table-all">
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+          {table.getFooterGroups().map(footerGroup => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+export default BasicTable;
